606a4d4d90066c14e43d1155be34fd983fd46fed
Vadimmmzz <91953115+Vadimmmzz@users.noreply.github.com>
2023-04-07 00:00:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import math
 
 from scipy.stats import norm
 
@@ -11,6 +10,6 @@ def solution(p: float, x: np.array) -> tuple:
     s=0
     for i in range (len(x)):
         s=s+(x[i]**2)
-    l=math.sqrt(s/np.quantile(x,1-p/2))/2
-    r=math.sqrt(s/np.quantile(x,p/2))/2
+    l=s/np.quantile(x,1-p/2)
+    r=s/np.quantile(x,p/2)
     return l,r
