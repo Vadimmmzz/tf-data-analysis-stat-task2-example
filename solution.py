cf2400f55fbfb9282f21da8463c9aac10d73d12d
Vadimmmzz <91953115+Vadimmmzz@users.noreply.github.com>
2023-04-07 22:14:29 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,18 +2,13 @@ import pandas as pd
 import numpy as np
 import math
 from scipy.stats import norm
-
+from scipy.stats import chi2
 
 chat_id = 230865321
 
 def solution(p: float, x: np.array) -> tuple:
-    s = 0
+    s = len(x)*(x*x).mean()
     a = 1 - p
-    for i in range(len(x)):
-        s = s + (x[i] ** 2)
-    hil = np.quantile(x, 1-a/2)
-    hir = np.quantile(x, a/2)
-    s = math.sqrt(s)
-    l = s/(2*hil)
-    r = s /(2*hir)
+    l = ((1/4)*s / chi2.ppf(1 - a/2, df = 2*len(x))) ** 0.5
+    r = ((1/4)*s / chi2.ppf(a/2, df = 2*len(x))) ** 0.5
     return l, r
