96402a86c5831b5d96b1d27223515327ff451a2d
Vadimmmzz <91953115+Vadimmmzz@users.noreply.github.com>
2023-04-07 19:23:19 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
-
+import math
 from scipy.stats import norm
 
 
 chat_id = 230865321
 
 def solution(p: float, x: np.array) -> tuple:
-    s=0
-    for i in range (len(x)):
-        s=s+(x[i]**2)
-    l=s/np.quantile(x,1-p/2)
-    r=s/np.quantile(x,p/2)
-    return l,r
+    s = 0
+    a = 1 - p
+    for i in range(len(x)):
+        s = s + (x[i] ** 2)
+    hil = np.quantile(x, 1-a/2)
+    hir = np.quantile(x, a/2)
+    s = math.sqrt(s)
+    l = s/(2*hil)
+    r = s /(2*hir)
+    return l, r
